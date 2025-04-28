f01eceedfc5aabb4b6474cd1ed764f54502a6e1c
HernanParma <hernanparma22@gmail.com>
2025-04-28 19:21:19 -0300

    Creacion de entidades y Modelado de tablas

--- Application/Interfaces/IQuery/Interface1.cs
+++ Application/Interfaces/IQuery/Interface1.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Interfaces.IQuery
+namespace Domain.Entities
 {
-    interface Interface1
+    public class Payment
     {
+        public Guid PaymentId { get; set; }
+        public DateTime Date { get; set; }
+        public string Amount { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+        public int PaymentMethodId { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public int PaymentStatusId { get; set; }
+        public IList<Reservation> Reservations { get; set; }
     }
 }
